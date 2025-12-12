d89738b9b6610660c62d165029158df9c4d5877a
ludwiga5 <aludwig5@outlook.com>
2025-12-12 10:45:50 -0500

    Initial commit - Spring Boot configured, launching, and localhost:8080 connected. SQLite dependency is included.

---
+++ backend/src/main/java/TriForm/TriFormApplication.java
@@ -0,0 +1,13 @@
+package TriForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriFormApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/TriForm/TriFormApplicationTests.java
@@ -0,0 +1,13 @@
+package TriForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
