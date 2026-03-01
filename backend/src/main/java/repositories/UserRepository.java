46d4dedcd0bae6db7cc862f3e92c463a713184b3
Alex Ludwig <aludwig5@outlook.com>
2026-02-28 19:46:31 -0500

    Added findByEmailOrUsername function

--- backend/src/main/java/repositories/UserRepository.java
+++ backend/src/main/java/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import entities.User;
@@ -8,6 +10,7 @@ public interface UserRepository extends JpaRepository <User, Long>{
     
     User findByUsername(String username);
     User findByEmail(String email);
+    Optional<User> findByEmailOrUsername(String email, String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 
