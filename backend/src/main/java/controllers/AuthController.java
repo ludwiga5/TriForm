91723de44076fc50ed2e6d04de2bc4a8989c5431
Alex Ludwig <aludwig5@outlook.com>
2026-02-28 19:46:04 -0500

    Adjusted login function to support a single-field login for Email/Username

--- backend/src/main/java/controllers/AuthController.java
+++ backend/src/main/java/controllers/AuthController.java
@@ -2,10 +2,17 @@ package controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import dto.JwtResponse;
 import entities.User;
+import exceptions.IncorrectPasswordException;
+import exceptions.UserNotFoundException;
+import services.JwtService;
 import services.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +24,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthController {
 
     private final UserService userService;
+    private final JwtService jwtService;
 
     //Injects
-    public AuthController(UserService userService){
+    public AuthController(UserService userService, JwtService jwtService){
         this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     //Registration Endpoint
@@ -37,8 +46,17 @@ public class AuthController {
 
     //Checks for registered user value. If the user exists it assigns a JWT token.
     @PostMapping("/login")
-    public JwtResponse login(@RequestBody User user){
-        JwtResponse loginToken = userService.loginUser(user);
-        return loginToken;
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        String identifier = request.get("identifier");
+        String password = request.get("password");
+        
+        try {
+            User user = userService.authenticateByIdentifier(identifier, password);
+            String token = jwtService.generateToken(user.getUsername());
+            JwtResponse response = new JwtResponse(token);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException | IncorrectPasswordException e) {
+            return ResponseEntity.status(401).body(Map.of("error", e.getMessage()));
+        }
     }
 }
