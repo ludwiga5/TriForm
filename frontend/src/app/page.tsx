3f5a75ab10889ff97fb1561bda326670ed6180ab
Alex Ludwig <aludwig5@outlook.com>
2026-03-04 07:27:07 -0500

    Landing page with Login created & tested

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,57 +1,81 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { PostRequest } from "@/lib/api-helper";
+import { text } from "stream/consumers";
 
+interface LoginResponse{
+    token: string;
+}
 
-export default function Home() {
+export default function Home(){
+    const [identifier, setIdentifier] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+      const handleSubmit = async (e: React.FormEvent) => {
+          e.preventDefault();
+  
+          const response = await PostRequest<LoginResponse>(
+              "/account/login", 
+              {identifier, password}
+          );
+  
+          //Check for and output Error
+          if (response.error){
+              console.log(response.error);
+              setErrorMessage(response.error);
+          }
+          //Passes and stores the JWT Token
+          if (response.data){
+              console.log("token: ", response.data.token);
+              localStorage.setItem("token", response.data.token);
+              setErrorMessage(null);
+          }
+      };
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/globe.svg"
-          alt="TriForm logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>Welcome to TriForm</h1>
-          <p>
-            Do you have an already have an account?
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="/login"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Login
-          </a>
-          <a
-            className={styles.secondary}
-            href="/register"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Sign Up
-          </a>
-        </div>
-        <div className={styles.intro}>
-          <h3>Want to follow the project?</h3>
+        <main className={styles.intro}>
+        <h1>Welcome to TriForm</h1>
+        <div className={styles.card}>
+          <form onSubmit={handleSubmit} className={styles.loginForm}>
+            <h2>Login</h2>
+            {errorMessage && <div className={styles.error}>{errorMessage}</div>}
+            <input 
+            className={styles.inputField}
+            type="text" 
+            value={identifier}
+            onChange={(e) => setIdentifier(e.target.value)} 
+            placeholder="Email or Username" 
+            required
+            />
+            <input 
+            className={styles.inputField}
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            placeholder="Password" 
+            required
+            />
+            <button className={styles.primaryButton} type="submit">Login</button>
+          </form>
           <div className={styles.ctas}>
-            <a
-            className={styles.secondary}
-            href="https://github.com/ludwiga5/TriForm"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Check out my GitHub
+            <a className={styles.secondary} href="/register">
+                Sign up
             </a>
           </div>
         </div>
-      </main>
+        <div className={styles.ctas}>
+          <a className={styles.primary}
+             href="https://github.com/ludwiga5/TriForm" 
+             target="_blank" 
+             rel="noopener noreferrer">
+            Check out the GitHub
+          </a>
+        </div>
+
+        </main>
     </div>
   );
 }
